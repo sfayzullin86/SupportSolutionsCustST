fe628911dcb68e1bfb66897a066ea330f1b8e19a
ljs <sfayzullin86@gmail.com>
2017-05-13 19:21:28 +0000

    autogeneration commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("srh.comsupportsolutionscustst.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("srh.comsupportsolutionscustst.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
